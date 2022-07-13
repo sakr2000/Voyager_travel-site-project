f8a4c267d7b93124e923463d6af87ba96a89bc82
mohamed_sakr <sakr83117@gmail.com>
2022-07-13 18:50:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fontawesome-iconpicker/3.2.0/js/fontawesome-iconpicker.min.js">
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <!-- Header section -->
+    <section class="header">
+        <a href="home.php" class="logo">Voyager</a>
+        <nav class="navbar">
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+        </nav>
+    </section>
+    
+
+
+<!-- Js files links -->
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
