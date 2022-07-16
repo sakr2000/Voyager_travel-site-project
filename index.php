5363c87a8c8b29ac7c405783af0f7525353b045f
mohamed_sakr <sakr83117@gmail.com>
2022-07-16 20:27:32 +0200

    header & footer

--- index.php
+++ index.php
@@ -24,6 +24,48 @@
         </nav>
         <div id="menu-btn" class="fas fa-bars"></div>
     </section>
+
+
+
+
+    <!-- footer section -->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+            <h3>quick links</h3>
+            <a href="index.php"><i class="fas fa-angle-right"></i> Home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> About</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i> Package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i> Book</a>
+            </div>
+
+            <div class="box">
+            <h3>Extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i>Ask a Question</a>
+            <a href="#"><i class="fas fa-angle-right"></i>About us</a>
+            <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i>Terms of use</a>
+            </div>
+
+            <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +201011****84</a>
+            <a href="#"><i class="fas fa-phone"></i> +201012****65</a>
+            <a href="#"><i class="fas fa-envelope"></i> sakr93117@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> Menofia,Egypt</a>
+            </div>
+
+            <div class="box">
+                <h3>Follow us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i> Linkedin</a>
+            </div>
+        </div>
+        
+        <div class="rights">Created by <span>M.Sakr</span> | All rights reserved</div>
+    </section>
     
 
 
