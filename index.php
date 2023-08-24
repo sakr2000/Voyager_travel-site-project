42957018fca5602e940d572c7efb761dc60a93d1
mohamed_sakr <sakr83117@gmail.com>
2023-08-24 07:08:01 +0300

    Added about & packages section

--- index.php
+++ index.php
@@ -61,23 +61,23 @@
             <div class="swiper-button-prev"></div>
         </div>
     </section>
-
+    
     <!-- services section  -->
     <section class="services">
-        <h1 class="heading-title"> Our Services</h1>
+        <h2 class="heading-title"> Our Services</h2>
         <div class="box-container">
-
+            
             <div class="box">
                 <img src="images/icon-1.png" alt="icon-1">
                 <h3>adventure</h3>
             </div>
             
-
+            
             <div class="box">
                 <img src="images/icon-2.png" alt="icon-2">
                 <h3>Tour guide</h3>
             </div>
-
+            
             <div class="box">
                 <img src="images/icon-3.png" alt="icon-3">
                 <h3>Trekking</h3>
@@ -100,7 +100,59 @@
 
         </div>
     </section>
+    
+    <!-- About section -->
+    <section class="home-about">
+        <div class="image">
+            <img src="images/about-img.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>About us</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, corrupti praesentium nostrum eveniet, porro perferendis temporibus vero modi facilis corporis libero dolore repellendus voluptates consequatur laboriosam inventore animi, quo numquam?</p>
+            <a href="about.php" class="btn">Read more</a>
+        </div>
+    </section>
+    <!-- Packages section -->
+    <section class="home-packages">
+        <h2 class="heading-title">Our Packages</h2>
+        <div class="box-container">
 
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-1.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>Adventure & tour</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ratione.</p>
+                    <a href="book.php" class="btn">Book now</a>
+                </div>
+            </div>
+            
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-2.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>Adventure & tour</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ratione.</p>
+                    <a href="book.php" class="btn">Book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-3.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>Adventure & tour</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ratione.</p>
+                    <a href="book.php" class="btn">Book now</a>
+                </div>
+            </div>
+            
+        </div>
+        <div class="load-more"><a href="package.php" class="btn">Load more</a></div>
+    </section>
     <!-- footer section -->
     <section class="footer">
         <div class="box-container">
@@ -139,7 +191,7 @@
         
         <div class="rights">Created by <span>M.Sakr</span> | All rights reserved</div>
     </section>
-    
+
     <!-- Js files links -->
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
     <script src="js/main.js"></script>
