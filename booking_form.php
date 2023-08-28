9d507fc0dd1dec9ccaa393be22a3ac2d8aea51b9
mohamed_sakr <sakr83117@gmail.com>
2023-08-28 14:40:01 +0300

    Store form values into phpMyAdmin DB

--- booking_form.php
+++ booking_form.php
@@ -1,76 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bokking</title>
+<?php
+$connection = mysqli_connect('localhost','root','','book_db');
+if ($connection->connect_error) {
+    die("Connection failed: ".$connection->connect_error);
+}
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $phone = $_REQUEST['phone'];
+    $location = $_REQUEST['location'];
+    $address = $_REQUEST['address'];
+    $guests = $_REQUEST['guests'];
+    $arrivals = $_REQUEST['arrivals'];
+    $leaving = $_REQUEST['leaving'];
 
-    <!-- swiper css link -->
-    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    
-</head>
-<body>
-    <!-- Header section -->
-    <section class="header">
-        <a href="index.php" class="logo">Voyager</a>
-        <nav class="navbar">
-            <a href="index.php">Home</a>
-            <a href="about.php">About</a>
-            <a href="package.php">Package</a>
-            <a href="book.php">Book</a>
-        </nav>
-        <div id="menu-btn" class="fas fa-bars"></div>
-    </section>
-
-
-
-
-    <!-- footer section -->
-    <section class="footer">
-        <div class="box-container">
-            <div class="box">
-            <h3>quick links</h3>
-            <a href="index.php"><i class="fas fa-angle-right"></i> Home</a>
-            <a href="about.php"><i class="fas fa-angle-right"></i> About</a>
-            <a href="package.php"><i class="fas fa-angle-right"></i> Package</a>
-            <a href="book.php"><i class="fas fa-angle-right"></i> Book</a>
-            </div>
-
-            <div class="box">
-            <h3>Extra links</h3>
-            <a href="#"><i class="fas fa-angle-right"></i>Ask a Question</a>
-            <a href="#"><i class="fas fa-angle-right"></i>About us</a>
-            <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
-            <a href="#"><i class="fas fa-angle-right"></i>Terms of use</a>
-            </div>
-
-            <div class="box">
-            <h3>contact info</h3>
-            <a href="#"><i class="fas fa-phone"></i> +201011****84</a>
-            <a href="#"><i class="fas fa-phone"></i> +201012****65</a>
-            <a href="#"><i class="fas fa-envelope"></i> sakr93117@gmail.com</a>
-            <a href="#"><i class="fas fa-map"></i> Menofia,Egypt</a>
-            </div>
-
-            <div class="box">
-                <h3>Follow us</h3>
-                <a href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
-                <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
-                <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
-                <a href="#"><i class="fab fa-linkedin"></i> Linkedin</a>
-            </div>
-        </div>
-        
-        <div class="rights">Created by <span>M.Sakr</span> | All rights reserved</div>
-    </section>
-    
-
-
-<!-- Js files links -->
-<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-<script src="js/main.js"></script>
-</body>
-</html>
+    $request = "insert into book_form(name,email,address,phone,location,guests,arrivals,leaving) values ('$name','$email','$address','$phone','$location','$guests','$arrivals','$leaving')";
+    if(mysqli_query($connection,$request)){
+        // Close connection
+        mysqli_close($connection);
+        header('location:book.php');
+    }else{
+        echo "ERROR: Hush! Sorry $request. "
+            . mysqli_error($connection);
+    }
+}
